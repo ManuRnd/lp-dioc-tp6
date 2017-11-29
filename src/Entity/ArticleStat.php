31fe865ee90146ace934c5d35c85feefb9d786bf
manuel.renaudineau <manuel_renaudineau@ens.univ-artois.fr>
2017-11-29 08:58:16 +0100

    premier commit

--- src/Entity/ArticleStat.php
+++ src/Entity/ArticleStat.php
@@ -12,13 +12,100 @@ class ArticleStat
     const CREATE = 'create';
     const UPDATE = 'update';
     const VIEW = 'view';
-
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $action;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Article")
+     */
     private $article;
+    /**
+     * @ORM\Column(type="datetime")
+     */
     private $date;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $ip;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $user;
 
+    /**
+     * ArticleStat constructor.
+     * @param $action
+     * @param $article
+     * @param $date
+     * @param $ip
+     * @param $user
+     */
+    public function __construct($action, $article, $date, $ip, $user)
+    {
+        $this->action = $action;
+        $this->article = $article;
+        $this->date = $date;
+        $this->ip = $ip;
+        $this->user = $user;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArticle()
+    {
+        return $this->article;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
     // Uniquement des getter et un constructeur
+
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,13 +12,128 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class User implements UserInterface, \Serializable
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     private $id;
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
     private $email;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $firstname;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $lastname;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $isAuthor = false;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Article")
+     */
     private $articles;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function isAuthor()
+    {
+        return $this->isAuthor;
+    }
+
+    /**
+     * @param mixed $isAuthor
+     */
+    public function setIsAuthor($isAuthor)
+    {
+        $this->isAuthor = $isAuthor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArticles()
+    {
+        return $this->articles;
+    }
+
+    /**
+     * @param mixed $articles
+     */
+    public function setArticles($articles)
+    {
+        $this->articles = $articles;
+    }
+
 
     // Fixme
 
